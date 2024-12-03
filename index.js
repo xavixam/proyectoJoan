6d369a470cf03caf3e5fe6d7789af7bd7055ae43
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-12-03 12:07:48 +0100

    Swagger hecho

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const { dbConnection } = require("./config/config")
+const swaggerUI = require('swagger-ui-express')
+const docs = require('./docs/index')
 
 app.use(express.json())
 
 app.use("/tasks",require("./routes/tasks"))
+
+app.use('/api-docs', swaggerUI.serve,swaggerUI.setup(docs))
 
 dbConnection()
 
